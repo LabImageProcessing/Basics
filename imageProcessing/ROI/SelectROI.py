08852fe16d6861ed735ca45f8e0a5ca4ac3901bc
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-04-28 17:55:53 +0900

    Update SelectROI.py

--- imageProcessing/ROI/SelectROI.py
+++ imageProcessing/ROI/SelectROI.py
@@ -1,16 +1,17 @@
 # ROI 실습
 # selectROI로 관심영역 지정 및 표시, 저장
 
-import cv2, numpy as np
+import cv2
+import numpy as np
 
 img = cv2.imread('.jpg')
+img_resize = cv2.resize(img, dsize=(500, 500), interpolation=cv2.INTER_AREA)  # 이미지 크기 조절
 
-x,y,w,h	= cv2.selectROI('img', img, False)
+x, y, w, h = cv2.selectROI('img', img_resize, False)
 if w and h:
-    roi = img[y:y+h, x:x+w]
+    roi = img_resize[y:y + h, x:x + w]
     cv2.imshow('cropped', roi)  # ROI 지정 영역을 새창으로 표시
-    cv2.moveWindow('cropped', 0, 0)  # 새창을 화면 좌측 상단으로 이동
-    cv2.imwrite('./cropped2.jpg', roi)   # ROI 영역 파일로 저장
+    cv2.imwrite('./cropped2.jpg', roi)  # ROI 영역 파일로 저장
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
